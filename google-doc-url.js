38ba1ee85e7a17c3889fffd8e259b6cd66e5b88a
MeganKSchneider <schnemae@uni.edu>
2017-05-12 11:03:17 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Aop6pmyaZ1vu8yCEvmyEJzlwLqYx8_0ZukuvAqSo2nw/edit#gid=164271551';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Aop6pmyaZ1vu8yCEvmyEJzlwLqYx8_0ZukuvAqSo2nw/edit#gid=0';
